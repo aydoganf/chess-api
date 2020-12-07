3fc460464d21a60c7362245dcdc3a6c4b1d2d510
Faruk Aydogan <aydoganf@itu.edu.tr>
2020-12-07 16:28:08 +0300

    ok?

--- ChessPlaying.API/Model/Request/CreateSessionRequest.cs
+++ ChessPlaying.API/Model/Request/CreateSessionRequest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Thrones.Gaming.Chess.Players;
 
 namespace ChessPlaying.API.Model.Request
